6a04d4f0881d025c5271a6c311ab63cb7126762f
rwald <robin.wald@hotmail.de>
2020-08-19 11:23:48 +0200

    course complete

---
+++ 08_Intermediate importing data in Python/Twitterkey.py
@@ -0,0 +1,19 @@
+class Twitterkey:
+
+    def __init__(self):
+        self.__access_token = "1251472497435951104-f8xVvovXJ74OWrnuqDMXBy5Hn6lEwg"
+        self.__access_token_secret = "bhMzW3jmsEGrbAewyxFS3Z9LYomWbtzOp0p43YEJX4zhl"
+        self.__consumer_key = "caFV0k2Kgtwt4Fnd1I26WK0Kj"
+        self.__consumer_secret = "QZPNvExUI16cIziVd3isdgEZdCSAszlwaHLYsOszj81jvtXQlU"
+
+    def get_access_token(self):
+        return self.__access_token
+
+    def get_access_token_secret(self):
+        return self.__access_token_secret
+
+    def get_consumer_key(self):
+        return self.__consumer_key
+
+    def get_consumer_secret(self):
+        return self.__consumer_secret
